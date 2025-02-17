98c35ae0ef417fd2d91e355bffaebf5b78782c2b
ryakkalauncc <163656008+ryakkalauncc@users.noreply.github.com>
2025-02-17 00:03:13 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -32,5 +32,11 @@ class Topic(models.Model):
         updated = models.DateTimeField(auto_now=True)
         created = models.DateTimeField(auto_now_add=True)
 
+        class Meta:
+            ordering = ['-updated', '-created']
+
+            def __str__(self):
+                return self.body[0:50]
+
         def __str__(self):
             return self.body[0:50]
