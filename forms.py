9cedf9fd2fd595f8131490cdf8fc6374d71df748
ryakkalauncc <163656008+ryakkalauncc@users.noreply.github.com>
2025-02-16 21:47:39 -0500

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from .models import Room
+from .forms import RoomForm
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude = ['host', 'participants']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['avatar', 'name', 'username', 'email', 'bio']
